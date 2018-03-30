211d5eb0f5ca1ce4fbe273f59aa46a0697490151
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-03-30 22:02:52 +0200

    V 1.1 include arithmetic operations

--- app/src/main/java/com/example/piotr/calculatorrpn/MainActivity.kt
+++ app/src/main/java/com/example/piotr/calculatorrpn/MainActivity.kt
@@ -11,85 +11,115 @@ import java.util.function.DoubleBinaryOperator
 class MainActivity : AppCompatActivity() {
 
     var stack = Stack()
-    var tmp : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        readButtons()
+        buttonsClick()
     }
-    fun readButtons(){
+    fun buttonsClick(){
         buttonZero.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 0" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,0)
+            stack.listTmp.add("0")
         }
 
         buttonOne.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 1" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,1)
+            stack.listTmp.add("1")
         }
 
         buttonTwo.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 2" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,2)
+            stack.listTmp.add("2")
         }
         buttonThree.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 3" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,3)
+            stack.listTmp.add("3")
         }
         buttonFour.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 4" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,4)
+            stack.listTmp.add("4")
         }
         buttonFive.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 5" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,5)
+            stack.listTmp.add("5")
         }
         buttonSix.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 6" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,6)
+            stack.listTmp.add("6")
         }
         buttonSeven.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 7" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,7)
+            stack.listTmp.add("7")
         }
         buttonEight.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 8" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,8)
+            stack.listTmp.add("8")
         }
         buttonNine.setOnClickListener{
-
             Toast.makeText(getApplicationContext(),"Kliknalem 9" ,Toast.LENGTH_LONG ).show();
-            tmp=makeNumberForStack(tmp,9)
+            stack.listTmp.add("9")
+        }
+        buttonDot.setOnClickListener{
+            Toast.makeText(getApplicationContext(),"Kliknalem ." ,Toast.LENGTH_LONG ).show();
+            stack.listTmp.add(".")
         }
         buttonEnter.setOnClickListener(){
-            stack.list.add(tmp)
-            val x = stack.list.get(stack.list.size-1)
-            Toast.makeText(getApplicationContext(),"$x" ,Toast.LENGTH_LONG ).show();
+            var y  = stack.listTmp.joinToString(separator = "")
+            stack.list.add(y.toDouble())
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+            stack.listTmp.clear()
+        }
+       buttonPlus.setOnClickListener(){
+           stack.addiction(stack)
+           Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+       }
+        buttonMinus.setOnClickListener(){
+            stack.subtraction(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+
+        buttonMultiply.setOnClickListener(){
+            stack.multiplication(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonDivision.setOnClickListener(){
+            stack.division(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonSqrt.setOnClickListener(){
+            stack.sqrt(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonPow.setOnClickListener(){
+            stack.pow(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonAC.setOnClickListener(){
+            stack.list.clear()
+            stack.listTmp.clear()
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonDrop.setOnClickListener(){
+            stack.drop(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonSwap.setOnClickListener(){
+            stack.swap(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
+        }
+        buttonPlusMinus.setOnClickListener(){
+            stack.plusMinus(stack)
+            Toast.makeText(getApplicationContext(), "stos " + stack.list ,Toast.LENGTH_LONG ).show();
         }
 
 
-    }
 
-    fun makeNumberForStack(a:Double, b:Int): Double{
-
-        val aPom = a.toInt()
-        val aStr= aPom.toString()
-        val sumStr = aStr+b.toString()
-        val result = sumStr.toDouble()
-        Toast.makeText(getApplicationContext(),"$result" ,Toast.LENGTH_LONG ).show();
-        return result
 
     }
+
+
+
 
 
 }
--- app/src/main/java/com/example/piotr/calculatorrpn/Stack.kt
+++ app/src/main/java/com/example/piotr/calculatorrpn/Stack.kt
@@ -1,6 +1,68 @@
 package com.example.piotr.calculatorrpn
 
+
 class Stack {
 
     var list: ArrayList<Double> = ArrayList()
+    var listTmp: ArrayList<String> = ArrayList()
+
+    fun addiction( stack : Stack){
+        val a = stack.list.get(stack.list.lastIndex)
+        val b = stack.list.get(stack.list.lastIndex-1)
+        val result = a+b
+        stack.list.removeAt(stack.list.lastIndex-1)
+        stack.list.removeAt(stack.list.lastIndex)
+        stack.list.add(result)
+    }
+    fun subtraction( stack : Stack){
+        val a = stack.list.get(stack.list.lastIndex)
+        val b = stack.list.get(stack.list.lastIndex-1)
+        val result = a-b
+        stack.list.removeAt(stack.list.lastIndex-1)
+        stack.list.removeAt(stack.list.lastIndex)
+        stack.list.add(result)
+    }
+    fun multiplication( stack : Stack){
+        val a = stack.list.get(stack.list.lastIndex)
+        val b = stack.list.get(stack.list.lastIndex-1)
+        val result = a*b
+        stack.list.removeAt(stack.list.lastIndex-1)
+        stack.list.removeAt(stack.list.lastIndex)
+        stack.list.add(result)
+    }
+    fun division( stack : Stack){
+        val a = stack.list.get(stack.list.lastIndex)
+        val b = stack.list.get(stack.list.lastIndex-1)
+        val result = a/b
+        stack.list.removeAt(stack.list.lastIndex-1)
+        stack.list.removeAt(stack.list.lastIndex)
+    }
+    fun sqrt(stack : Stack){
+        val a = stack.list.get(stack.list.lastIndex)
+        val result = Math.sqrt(a)
+        stack.list.removeAt(stack.list.lastIndex)
+        stack.list.add(result)
+    }
+    fun pow(stack:Stack) {
+        val a = stack.list.get(stack.list.lastIndex) //potega
+        val b = stack.list.get(stack.list.lastIndex - 1)
+        val result = Math.pow(b, a)
+        stack.list.removeAt(stack.list.lastIndex - 1)
+        stack.list.removeAt(stack.list.lastIndex)
+        stack.list.add(result)
+    }
+    fun drop (stack:Stack){
+        stack.list.removeAt(stack.list.lastIndex)
+    }
+    fun swap(stack:Stack){
+        val a = stack.list.get(stack.list.lastIndex) //potega
+        val b = stack.list.get(stack.list.lastIndex - 1)
+        stack.list.set(stack.list.lastIndex - 1,a)
+        stack.list.set(stack.list.lastIndex ,b)
+    }
+    fun plusMinus(stack:Stack){
+        val a = -(stack.list.get(stack.list.lastIndex) )
+        stack.list.set(stack.list.lastIndex,a)
+    }
+
 }
